5548517d48e3c2033e0d3e0c11f8a2d11f33e6d7
Felixiose <vittorifelix@gmail.com>
2022-12-21 17:52:56 +0100

    l

--- data/scRNADataset.py
+++ data/scRNADataset.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Ondrej Skopek.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
 from typing import Dict, Tuple, Optional, Any
 import numpy as np
@@ -24,11 +10,16 @@ from torch.utils.data import DataLoader
 from vae_dataset import VAEDataset
 
 
+from util.util import read_mtx
 
 class scRNADataset(torch.utils.data.Dataset):
 
-    def __init__(self, dim: int)
-        self.dim = int(dim)
+    def __init__(self, directory: str) -> None:
+        
+        x = read_mtx(directory).transpose().todense()
+
+
+
 
         self.data -= np.mean(self.data, axis=0, keepdims=True)
         self.data /= np.std(self.data, axis=0, keepdims=True)
